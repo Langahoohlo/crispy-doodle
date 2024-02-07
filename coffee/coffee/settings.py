d302bc73a14970516c28bf49c444384acb01840d
langahoohlo <langahoohlo@gmail.com>
2024-02-07 18:55:19 +0200

    bug fixes on images

--- coffee/coffee/settings.py
+++ coffee/coffee/settings.py
@@ -75,13 +75,20 @@ WSGI_APPLICATION = 'coffee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'coffeedb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Langisto17',
+#         'HOST': 'localhost',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'coffeedb',
-        'USER': 'postgres',
-        'PASSWORD': 'Langisto17',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
